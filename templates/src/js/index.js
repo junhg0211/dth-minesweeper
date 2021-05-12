0dd437a7b04923b83cc96eaf6071fa8370350dfa
junhg0211 <junhg0211@gmail.com>
2021-05-12 15:39:34 +0900

    javascript basis implementation

--- templates/src/js/index.js
+++ templates/src/js/index.js
@@ -1,18 +1,49 @@
-const canvas = document.getElementById("main-canvas");
+const fps = 30;
+
+const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 
-var width = canvas.clientWidth
-  , height = canvas.clientHeight;
+let rowCount = 9, gameHeight;
+let gameX, gameY;
+let cellSize;
 
-function refreshSize() {
-    canvas.width = width;
-    canvas.height = height;
+let horizontalPadding = 360,
+    verticalPadding = 50;
+
+function resize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    gameHeight = Math.min(canvas.height - verticalPadding * 2, canvas.width - horizontalPadding * 2);
+    cellSize = gameHeight / rowCount;
+
+    gameX = (canvas.width - gameHeight) / 2;
+    gameY = (canvas.height - gameHeight) / 2;
 }
-refreshSize();
 
-context.fillStyle = '#fdde59';
-context.fillRect(0, 0, width, height);
+function tick() {
 
-context.font = "64px Georgia";
-context.fillStyle = "#2c2c2c";
-context.fillText("Hello, wolrd!", 0, 100);
+}
+
+function render() {
+    context.fillStyle = 'lightgray';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    context.strokeStyle = 'black';
+    context.strokeRect(gameX, gameY, gameHeight, gameHeight);
+}
+
+window.addEventListener('resize', resize);
+resize();
+
+setInterval(() => {
+    tick();
+    render();
+}, 1000 / fps)
+
+// context.fillStyle = '#fdde59';
+// context.fillRect(0, 0, width, height);
+//
+// context.font = "64px Georgia";
+// context.fillStyle = "#2c2c2c";
+// context.fillText("Hello, wolrd!", 0, 100);
